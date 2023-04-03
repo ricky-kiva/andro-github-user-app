79b72619a080818b37d0ebb3310f9cc26bfa9c94
ricky-kiva <ricky.kiva@gmail.com>
2023-04-04 00:57:05 +0700

    add FavUser Repository

---
+++ app/src/main/java/com/rickyslash/githubuserapp/helper/ViewModelFactory.kt
@@ -0,0 +1,33 @@
+package com.rickyslash.githubuserapp.helper
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.rickyslash.githubuserapp.DetailUserViewModel
+
+class ViewModelFactory private constructor(private val mApplication: Application): ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailUserViewModel::class.java)) {
+            return DetailUserViewModel(mApplication) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ViewModelFactory? = null
+
+        @JvmStatic
+        fun getInstance(application: Application): ViewModelFactory {
+            if (INSTANCE == null) {
+                synchronized(ViewModelFactory::class.java) {
+                    INSTANCE = ViewModelFactory(application)
+                }
+            }
+            return INSTANCE as ViewModelFactory
+        }
+    }
+
+}
---
+++ app/src/main/java/com/rickyslash/githubuserapp/repository/FavUserRepository.kt
@@ -0,0 +1,43 @@
+package com.rickyslash.githubuserapp.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.rickyslash.githubuserapp.database.FavUser
+import com.rickyslash.githubuserapp.database.FavUserDao
+import com.rickyslash.githubuserapp.database.FavUserRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class FavUserRepository(application: Application) {
+    private val mFavUserDao: FavUserDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val db = FavUserRoomDatabase.getDatabase(application)
+        mFavUserDao = db.favUserDao()
+    }
+
+    fun getAllFavUsers(): LiveData<List<FavUser>> = mFavUserDao.getAllFavUsers()
+
+    fun insert(favUser: FavUser) {
+        executorService.execute { mFavUserDao.insert(favUser) }
+    }
+
+    fun update(favUser: FavUser) {
+        executorService.execute { mFavUserDao.update(favUser) }
+    }
+
+    fun delete(favUser: FavUser) {
+        executorService.execute { mFavUserDao.delete(favUser) }
+    }
+
+    fun getFavUserByUsername(username: String): LiveData<FavUser> = mFavUserDao.getFavUserByUsername(username)
+
+    fun insertFavUser(username: String, avatarUrl: String) {
+        executorService.execute { mFavUserDao.insertFavUser(username, avatarUrl) }
+    }
+
+    fun deleteFavUserByUsername(username: String) {
+        executorService.execute { mFavUserDao.deleteFavUserByUsername(username) }
+    }
+}
